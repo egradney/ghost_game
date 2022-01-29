3698c4d98cbdc63093515241ea7cc74f81321679
Elli Gradney <elli@elligradney.io>
2022-01-28 23:14:55 -0600

    improving AiPlayer logic'

--- ai_player.rb
+++ ai_player.rb
@@ -11,12 +11,35 @@ class AiPlayer
         @losses = 0
     end
 
-    def guess
-        alpha = 'abcdefghijklmnopqrstuvwxyz'
-        input = alpha[rand(26)]
-         
-    end
+    def guess(fragment, fragment_length, number_of_current_players, possible_words)
+       
+        if possible_words.any? { |possible| possible.length < fragment_length + number_of_current_players }
 
+            winners = possible_words.select { |possible| possible.length < fragment_length + 1 + number_of_current_players }
+            possible_letters = winners.map { |possible_word| possible_word[fragment_length]}
+            if possible_words.length < 10
+                print possible_words
+            end
 
+            possible_winning_letters = possible_letters.select { |letter| !possible_words.include?("#{fragment}#{letter}") }
+
+            if possible_winning_letters.length > 0
+                puts possible_winning_letters
+                puts 'random winning move'
+                random_winning_move = possible_winning_letters.sample
+                input = random_winning_move
+            else
+                puts 'random losing move'
+                random_losing_move = possible_letters.sample
+                input = random_losing_move
+            end
+            
+        else
+            puts 'random move'
+            random_move = possible_words.sample[fragment_length]
+            input = random_move
+        end
+
+    input
 
 end
--- game.rb
+++ game.rb
@@ -19,6 +19,7 @@ class Game
         @current_player = @players[0]
         @previous_player = @players[1]
         @dictionary = File.readlines("dictionary.txt").map(&:chomp)
+        @number_of_current_players = @players.length
 
     end
 
@@ -42,15 +43,19 @@ class Game
 
         else
 
-            frag_add = @current_player.guess
-
+            possible_words = @dictionary.select { |word| word.start_with?(@fragment) }
+            fragment_length = @fragment.length
+            frag_add = @current_player.guess(@fragment, fragment_length, @number_of_current_players, possible_words )
+            
             if self.valid_play?(frag_add)
+
                 @fragment += frag_add
                 puts "\n #{@current_player.name}'s contribution is '#{frag_add}'"
                 puts "\n new fragment is: '#{@fragment}'"
                 if !self.lose?(@fragment)
                     self.switch_player
                 end
+
             else
                 self.take_turn
             end
@@ -127,6 +132,7 @@ class Game
                 when 5
                     puts '---------------------'
                     print "\n #{@current_player.name} has 'GHOST!' \n They're out!\n"
+                    @number_of_current_players += -1
                     if self.win?
                         winner = @players.select { |player| player.losses < 5 }
                         print "\n#{winner[0].name} WINS!\n"
@@ -155,3 +161,39 @@ class Game
 end
 
 Game.new('Will' => 'human', 'Jess'=> 'human', 'Tammy' => 'ai').run
+
+
+
+# possible_words = @dictionary.select { |word| word.start_with?(@fragment) }
+# fragment_length = @fragment.length
+
+
+# # attempt to find moves that will force opponents to complete a word
+# # by finding words that can be completed before AiPlayer's next turn
+# if possible_words.any? { |possible| possible.length < fragment_length + @number_of_current_players }
+#     winners = possible_words.select { |possible| possible.length < @fragment.length + 1 + @number_of_current_players }
+#     possible_letters = winners.map { |possible_word| possible_word[fragment_length]}
+#     random_winning_move = possible_letters.sample
+#     frag_add = random_winning_move
+# else
+
+#     frag_add = @current_player.guess
+
+
+#         if self.valid_play?(frag_add)
+
+
+#             @fragment += frag_add
+#             puts "\n #{@current_player.name}'s contribution is '#{frag_add}'"
+#             puts "\n new fragment is: '#{@fragment}'"
+#             if !self.lose?(@fragment)
+#                 self.switch_player
+#             end
+
+#         else
+#             self.take_turn
+#         end
+
+#     end
+
+# end
